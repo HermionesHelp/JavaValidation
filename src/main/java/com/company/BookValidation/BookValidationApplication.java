b1be9bb2b7e7ad01e8905134f992fef29e2dfb32
Kristan Frost <kristan.frost@gmail.com>
2022-05-19 12:10:32 -0500

    first commit

---
+++ src/main/java/com/company/BookValidation/BookValidationApplication.java
@@ -0,0 +1,13 @@
+package com.company.BookValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/BookValidation/BookValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.BookValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
